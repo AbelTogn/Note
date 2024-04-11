09a9bfe2f35fff3f61ca373f305be0601c5c0547
AbelTogn <abeltognonipro@gmail.com>
2024-04-11 15:08:06 +0200

    add

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,8 +1,11 @@
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,6 +17,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.AddCircle
 import androidx.compose.material.icons.twotone.Info
 import androidx.compose.material.icons.twotone.Search
 import androidx.compose.runtime.Composable
@@ -26,7 +30,10 @@ import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+data class Note(var title: String, var text: String)
+
 val roundedIcons = Icons.TwoTone
+val buttonBackground = Color(59,59,59)
 
 @Preview
 @Composable
@@ -46,49 +53,76 @@ fun App() {
 @Preview
 @Composable
 fun Home(){
-    val buttonBackground = Color(59,59,59)
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(0.dp, 47.dp)
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Notes",
+        Row(
             modifier = Modifier
-                .padding(24.dp, 0.dp, 0.dp, 0.dp),
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            fontSize = 43.sp
-        )
-        Button(
-            onClick = {},
-            border = BorderStroke(1.dp, Color.Black),
-            shape = RoundedCornerShape(15),
-            colors = ButtonDefaults.buttonColors(buttonBackground),
-            modifier = Modifier
-                .size(width = 50.dp, height = 50.dp)
+                .fillMaxWidth()
+                .padding(0.dp, 47.dp)
         ) {
-            Icon(
-                roundedIcons.Search,
-                contentDescription = null,
+            Text(
+                text = "Notes",
                 modifier = Modifier
-                    .size(width = 24.dp, height = 24.dp)
+                    .padding(24.dp, 0.dp, 0.dp, 0.dp),
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 43.sp
             )
+
+            Spacer(modifier = Modifier.size(width = 140.dp, height = 0.dp))
+
+            Button(
+                onClick = {},
+                border = BorderStroke(1.dp, Color.Black),
+                shape = RoundedCornerShape(15),
+                colors = ButtonDefaults.buttonColors(buttonBackground),
+                modifier = Modifier
+                    .size(width = 50.dp, height = 50.dp)
+            ) {
+                Icon(
+                    roundedIcons.Search,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(width = 24.dp, height = 24.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.size(width = 20.dp, height = 0.dp))
+
+            Button(
+                onClick = {},
+                border = BorderStroke(1.dp, Color.Black),
+                shape = RoundedCornerShape(15),
+                colors = ButtonDefaults.buttonColors(buttonBackground),
+                modifier = Modifier
+                    .size(width = 50.dp, height = 50.dp)
+            ) {
+                Icon(
+                    roundedIcons.Info,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(width = 24.dp, height = 24.dp)
+                )
+            }
         }
-        Button(
-            onClick = {},
-            border = BorderStroke(1.dp, Color.Black),
-            shape = RoundedCornerShape(15),
-            colors = ButtonDefaults.buttonColors(buttonBackground),
+        Row(
+            horizontalArrangement = Arrangement.End,
             modifier = Modifier
-                .size(width = 50.dp, height = 50.dp)
+                .fillMaxWidth()
+                .padding(0.dp, 400.dp, 20.dp, 0.dp)
         ) {
-            Icon(
-                roundedIcons.Info,
-                contentDescription = "",
-                modifier = Modifier
-                    .size(width = 24.dp, height = 24.dp)
-            )
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(buttonBackground),
+                shape = RoundedCornerShape(50),
+                modifier = Modifier.size(50.dp, 50.dp)
+            ) {
+                Icon(
+                    roundedIcons.AddCircle,
+                    contentDescription = null,
+                )
+            }
         }
     }
 }
@@ -97,14 +131,21 @@ fun Home(){
 @Preview
 @Composable
 fun Empty(){
-    Image(
-        painter = painterResource(R.drawable.emptyhome),
-        contentDescription = "Forest Image",
-    )
-
-    Text(
-        text = "Create your first note !",
+    Row(
+        horizontalArrangement = Arrangement.Center,
         modifier = Modifier
-            .padding(109.dp, 576.dp, 110.dp, 293.dp)
-    )
+            .padding(0.dp, 283.dp, 0.dp, 0.dp)
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.emptyhome),
+            contentDescription = "Forest Image",
+        )
+
+        Text(
+            text = "Create your first note !",
+        )
+    }
 }
+
+
